d9fc817d266b109c7dddecfc73d267cf055a47d5
andiva-mira <andiva.mira@gmail.com>
2018-05-21 15:11:31 +0100

    mobile menu functionality and styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,5 @@
 const $ = require('jquery');
-import Person from './modules/Person';
-//const Person = require('./modules/Person');
 
-//alert('This is a test for our webpack automation');
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-const john = new Person('John Smith', 'red');
-john.greet();
-
-const jane = new Adult('Jane Doe', 'blue');
-jane.greet();
-jane.payTaxes();
-
-$('h1').css({fontWeight: 'bold', color: '#ffffff'});
+const mobileMenu = new MobileMenu();
