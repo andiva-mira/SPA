2da80efa3ad49e1a235dcdcbe6844f9712d691c5
andiva-mira <andiva.mira@gmail.com>
2018-05-21 16:54:11 +0100

    Sticky Header behaviour

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
+const stickyHeader = new StickyHeader();
 
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+
+
