11236bdcedfd571693f6293cd9de06d436898db0
andiva-mira <andiva.mira@gmail.com>
2018-05-21 16:05:52 +0100

    Completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
-const $ = require('jquery');
-
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '60%');
